e83de1beb3dde0c2b2ed33670836e34cf01c47ae
brian-dg <brian_rc6@hotmail.com>
2022-03-10 12:42:24 -0300

    as

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/destacados":["static\u002Fchunks\u002Fpages\u002Fdestacados.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdestacados"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
